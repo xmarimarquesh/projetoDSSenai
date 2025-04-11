7703356ec8b53473801b1f9f08cba600d7a90736
juupurcino <jdiaspurcino@gmail.com>
2025-04-11 13:56:46 -0300

    Toma helena

--- backend/src/errors/AppError.ts
+++
@@ -1,8 +0,0 @@
-export default class AppError extends Error {
-    public statusCode: number;
-
-    constructor(message: string, statusCode: number) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}

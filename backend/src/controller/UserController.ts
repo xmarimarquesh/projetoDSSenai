2eb060214374ace49516f9f316041a5d849e6f9b
juupurcino <jdiaspurcino@gmail.com>
2025-04-11 14:27:18 -0300

    oi

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -8,42 +8,46 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class UserController {
-  static async createUser(req: Request, res: Response) {
-    const { name, email, username, password } = req.body;
 
-    try {
-      const user = await UserService.createUser(name, email, username, password);
-      return res.status(201).json(user);
-    } catch (error) {
-      return res.status(400).json({ message: 'Erro ao criar usuário', error });
+    static async createUser(req: Request, res: Response) {
+        const { name, email, username, password } = req.body;
+
+        try {
+            const user = await UserService.createUser(name, email, username, password);
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(400).json({ message: 'Erro ao criar usuário', error });
+        }
     }
-  }
 
-  static async login(req: Request, res: Response) {
-    const { email, password } = req.body;
+    static async login(req: Request, res: Response) {
+        const { email, password } = req.body;
 
-    try {
-      const user = await UserService.findUserByEmail(email);
+        try {
+            const user = await UserService.findUserByEmail(email);
 
-      if (!user) return res.status(400).json({ message: "Email inválido" });
+            if (user) {
 
-      const bytes = crypto.AES.decrypt(user.password, process.env.SECRET as string);
-      const passwordDecrypted = bytes.toString(crypto.enc.Utf8);
+                const bytes = crypto.AES.decrypt(user.password, process.env.SECRET as string);
+                const passwordDecrypted = bytes.toString(crypto.enc.Utf8);
 
-      if (password !== passwordDecrypted) {
-        return res.status(400).json({ message: "Senha inválida" });
-      }
+                if (password !== passwordDecrypted) {
+                    res.status(400).json({ message: "Senha inválida" });
+                }
 
-      const token = jwt.sign({ id: user.id }, process.env.SECRET as string, {
-        expiresIn: '2 days'
-      });
+                const token = jwt.sign({ id: user.id }, process.env.SECRET as string, {
+                    expiresIn: '2 days'
+                });
 
-      return res.status(200).json({ token });
+                res.status(200).json({ token });
+            }
 
-    } catch (error) {
-      return res.status(500).json({ message: "Erro interno", error });
+            res.status(400).json({ message: "Email inválido" });
+
+        } catch (error) {
+            res.status(500).json({ message: "Erro interno", error });
+        }
     }
-  }
 }
 
 export default UserController;
